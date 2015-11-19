d7d604b82faf846f1aa04843da41adc837f6ac68
pgilmorepf <paul@playfab.com>
2015-11-19 09:33:38 -0800

    Cocos Revising the time-stamp checking to avoid assumptions involving multiple server timestamps. T495
    Summary:
    Cocos
    Revising the time-stamp checking to avoid assumptions involving multiple server timestamps.
    T495
    
    Test Plan: This is the test
    
    Reviewers: Marco.Williams
    
    Reviewed By: Marco.Williams
    
    Maniphest Tasks: T495
    
    Differential Revision: https://phab.playfabdev.com/D177

--- PlayFabServerSDK/PlayFabSDK/source/PlayFabVersion.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "playfab/PlayFabVersion.h"
-
-namespace PlayFab
-{
-    const std::string PlayFabSDKVersion = "0.0.151109";
-    const std::string PlayFabVersionString = "Cocos2d-xSDK-0.0.151109";
-}
--- PlayFabServerSDK/include/playfab/PlayFabSettings.h
+++ PlayFabServerSDK/include/playfab/PlayFabSettings.h
@@ -10,29 +10,21 @@ namespace PlayFab
     class PlayFabSettings
     {
     public:
-
         static bool useDevelopmentEnvironment;
         static std::string serverURL;
         static std::string developmentEnvironmentURL;
         static std::string productionEnvironmentURL;
-		static std::string logicServerURL;
+
         static std::string titleId;
         static ErrorCallback globalErrorHandler;
-		static std::string developerSecretKey;
-		
+        static std::string developerSecretKey;
+
         static std::string getURL(const std::string& callPath)
         {
             if (serverURL.length() == 0)
                 serverURL = "https://"+titleId+(useDevelopmentEnvironment ? developmentEnvironmentURL : productionEnvironmentURL);
             return serverURL + callPath;
         }
-		
-		static std::string getLogicURL(const std::string& callPath)
-		{
-			return logicServerURL + callPath;
-		}
     };
-
-
 }
 #endif
