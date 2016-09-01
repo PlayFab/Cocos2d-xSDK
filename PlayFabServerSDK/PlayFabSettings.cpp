44f0efadd4b9c944d7d302887efd9df4fe4cfe15
playfabjenkinsbot <playfabjenkinsbot@github.com>
2016-09-01 17:47:54 +0000

    Automated pf-main build from Jenkins

--- PlayFabServerSDK/PlayFabSettings.cpp
+++ PlayFabServerSDK/PlayFabSettings.cpp
@@ -6,7 +6,7 @@ namespace PlayFab
     IHttpRequester* PlayFabSettings::httpRequester = new HttpRequesterCURL();
 
     const std::string PlayFabSettings::sdkVersion = "0.9.160829";
-    const std::string PlayFabSettings::buildIdentifier = "jbuild_cocos2d-xsdk_0";
+    const std::string PlayFabSettings::buildIdentifier = "jbuild_cocos2d-xsdk_1";
     const std::string PlayFabSettings::versionString = "Cocos2d-xSDK-0.9.160829";
 
     bool PlayFabSettings::useDevelopmentEnvironment = false;
